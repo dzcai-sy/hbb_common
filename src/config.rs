ce2c1854d1277370f09ba028d8cc99ea22587dbc
dzcai-sy <842439592@qq.com>
2025-05-22 11:28:16 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["172.31.1.17"];
+pub const RS_PUB_KEY: &str = "edEeW3wGu4F9gRmFW8WjTI1us9AIIVhKEhqqXI63RNc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
